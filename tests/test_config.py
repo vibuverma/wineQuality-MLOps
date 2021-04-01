456193a195df7364c89256461a6dc69c4275f030
vibuverma <vaibhavverma016@gmail.com>
2021-04-01 19:11:56 +0530

    Added notebook file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,12 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in range"):
+        self.message= message
+        super().__init__(self.message)
+
 def test_generic():
-    a=10
-    b=10
-    assert a == b
+    a=5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
